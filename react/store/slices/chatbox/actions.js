df20ce2d4b0eaeb2e799e43e15c9b338e7d53520
pavel_shevchuk_d <pavel.shevchuk@sitesoft.ru>
2021-04-04 13:26:00 +0000

    fix

--- react/store/slices/chatbox/actions.js
+++ react/store/slices/chatbox/actions.js
@@ -1,26 +1,23 @@
 import { connect, send } from '@giantmachines/redux-websocket';
+import config from "../../../../config";
 
 /**
  * Middleware functions
  */
 
 /**
- *Get data from Websocket middleware
+ * Get data from Websocket middleware
+ * (connect) Allows automatic reconnect after closed socket
  * @returns {Function}
  */
-export const socketOpenConnection = () => async (dispatch) => {
-  /**
-   * (connect) Allows automatic reconnect after closed socket
-   */
-  dispatch(connect('ws://skade.cc:38080'));
-};
+export const socketOpenConnection = () => async dispatch => dispatch(connect('ws://skade.cc:38080'));
 /**
  * @param message {string}
  * @returns {(function(*): Promise<void>)|*}
  */
 export const socketSend = (message) => async (dispatch) => {
   dispatch(send({
-    name:'mr верстальщик',
+    name:config.username,
     message:message
   }));
 };
